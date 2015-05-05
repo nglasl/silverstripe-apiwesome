19e54ca618cfdcdd857b9371329d0a9e6345523c
Nathan Glasl <nathan@silverstripe.com.au>
2015-05-05 15:36:14 +1000

    Updating some comments.

--- code/services/APIwesomeService.php
+++ code/services/APIwesomeService.php
@@ -250,7 +250,7 @@ class APIwesomeService {
 			}
 			else {
 
-				// This is a custom JSON output, therefore pass it directly through.
+				// This is a custom JSON output, therefore pass it through directly.
 
 				unset($object['ClassName']);
 				$temporary[] = array($class => $object);
@@ -417,7 +417,7 @@ class APIwesomeService {
 			}
 			else {
 
-				// This is a custom XML output, therefore pass it directly through.
+				// This is a custom XML output, therefore pass it through directly.
 
 				unset($object['ClassName']);
 				$this->recursiveXML($objectXML, $object);
